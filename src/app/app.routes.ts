ad73d2cb6ba4af3d949c0cc53b886c5625b2c7bd
Jhayrolandero <xjaylandero@gmail.com>
2024-12-15 11:00:48 +0800

    started login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ApplicationFormComponent } from './components/application-form/application-form.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
     {path: 'dashboard', component: DashboardComponent},
     {path: 'application', component: ApplicationFormComponent},
+    {path: 'login', component: LoginComponent},
 ];
