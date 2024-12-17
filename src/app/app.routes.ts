4faccd1bbda9e4cd1ba5220ffedc126c1bfc0605
Cjay Landero <xjaylandero@gmail.com>
2024-12-17 08:40:00 +0800

    done view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ApplicationFormComponent } from './components/application-form/application-form.component';
 import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
-    {path: '', component: DashboardComponent},
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'application', component: ApplicationFormComponent},
+    {path: '', component: LayoutComponent, children: [
+      {path: '', component: DashboardComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'application', component: ApplicationFormComponent},
+    ]},
     {path: 'login', component: LoginComponent},
 ];
---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    SidebarComponent
+
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
