555253bc643daf178ebd8b07a1f13e8184af796f
Cjay Landero <xjaylandero@gmail.com>
2025-01-07 21:16:37 +0800

    validation

--- src/app/components/application-form/application-form.component.ts
+++ src/app/components/application-form/application-form.component.ts
@@ -8,6 +8,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import { RequestService } from '../../service/request.service';
 import {MatRadioModule} from '@angular/material/radio';
 import { Information, LoanApplication, LoanDetails } from '../../interface';
+import { SnackbarService } from '../../service/snackbar.service';
 
 @Component({
   selector: 'app-application-form',
@@ -27,7 +28,10 @@ import { Information, LoanApplication, LoanDetails } from '../../interface';
 })
 export class ApplicationFormComponent {
 
-  constructor(private requestService: RequestService) {}
+  constructor(
+    private requestService: RequestService,
+    private snackbarService: SnackbarService
+  ) {}
 
   purposeArr = [
     "Hospitalization/Medical",
@@ -40,57 +44,68 @@ export class ApplicationFormComponent {
   ]
 
   loanDetailsForm = new FormGroup({
-    loanAmount: new FormControl(0),
-    loanNumber: new FormControl(''),
-    purpose: new FormControl(''),
-    term: new FormControl(0),
-    loanType: new FormControl('')
+    loanAmount: new FormControl(0, [Validators.required]),
+    loanNumber: new FormControl('', [Validators.required]),
+    purpose: new FormControl('', [Validators.required]),
+    otherPurpose: new FormControl(''),
+    term: new FormControl(0, [Validators.required]),
+    loanType: new FormControl('', [Validators.required])
   })
 
   borrowerInfoForm = new FormGroup({
-    lastName: new FormControl(''),
-    firstname: new FormControl(''),
+    lastName: new FormControl('', [Validators.required]),
+    firstname: new FormControl('', [Validators.required]),
     middleName: new FormControl(''),
-    region: new FormControl(''),
-    province: new FormControl(''),
-    city: new FormControl(''),
-    barangay: new FormControl(''),
-    street: new FormControl(''),
-    zipcode: new FormControl(''),
-    employeeNo: new FormControl(''),
-    employeeStatus: new FormControl(''),
-    birth: new FormControl(new Date()),
+    region: new FormControl('', [Validators.required]),
+    province: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    barangay: new FormControl('', [Validators.required]),
+    street: new FormControl('', [Validators.required]),
+    zipcode: new FormControl('', [Validators.required]),
+    employeeNo: new FormControl('', [Validators.required]),
+    employeeStatus: new FormControl('', [Validators.required]),
+    birth: new FormControl(new Date(), [Validators.required]),
     age: new FormControl(0),
-    office: new FormControl(''),
-    salary: new FormControl(''),
-    officeTelNo: new FormControl(''),
-    yearService: new FormControl(0),
-    mobileNo: new FormControl(''),
+    office: new FormControl('', [Validators.required]),
+    salary: new FormControl('', [Validators.required]),
+    officeTelNo: new FormControl('', [Validators.required]),
+    yearService: new FormControl(0, [Validators.required]),
+    mobileNo: new FormControl('' , [Validators.required]),
   })
-
   comakerInfoForm = new FormGroup({
-    lastName: new FormControl(''),
-    firstname: new FormControl(''),
+    lastName: new FormControl('', [Validators.required]),
+    firstname: new FormControl('', [Validators.required]),
     middleName: new FormControl(''),
-    region: new FormControl(''),
-    province: new FormControl(''),
-    city: new FormControl(''),
-    barangay: new FormControl(''),
-    street: new FormControl(''),
-    zipcode: new FormControl(''),
-    employeeNo: new FormControl(''),
-    employeeStatus: new FormControl(''),
-    birth: new FormControl(new Date()),
+    region: new FormControl('', [Validators.required]),
+    province: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    barangay: new FormControl('', [Validators.required]),
+    street: new FormControl('', [Validators.required]),
+    zipcode: new FormControl('', [Validators.required]),
+    employeeNo: new FormControl('', [Validators.required]),
+    employeeStatus: new FormControl('', [Validators.required]),
+    birth: new FormControl(new Date(), [Validators.required]),
     age: new FormControl(0),
-    office: new FormControl(''),
-    salary: new FormControl(''),
-    officeTelNo: new FormControl(''),
-    yearService: new FormControl(0),
-    mobileNo: new FormControl(''),
+    office: new FormControl('', [Validators.required]),
+    salary: new FormControl('', [Validators.required]),
+    officeTelNo: new FormControl('', [Validators.required]),
+    yearService: new FormControl(0, [Validators.required]),
+    mobileNo: new FormControl('' , [Validators.required]),
   })
 
+  isLinear = true;
 
-  isLinear = false;
+  purposeChange(type: string) {
+    if(type === 'main') {
+      this.loanDetailsForm.patchValue({
+        otherPurpose: ''
+      })
+      console.log('asdsd')
+    } else if(type === 'other') {
+      console.log('asdsd')
+      this.loanDetailsForm.get('purpose')?.setValue("NA");
+    }
+  }
 
   parseForm() {
     const applicationForm: LoanApplication = {
@@ -100,6 +115,44 @@ export class ApplicationFormComponent {
     }
 
     return applicationForm
+  }
+
+  private parseMessage(words: string[]) {
+    let message = ''
+
+    if(words.length < 2) {
+      message = `Recheck ${words} is input`
+    } else if(words.length < 4) {
+      message = `Recheck  ${words}inputs`
+    }else {
+      message = `Some inputs are invalid`
+    }
+
+    this.snackbarService.showSnackbar(message)
+  }
+
+  findInvalidControls(formGroup: FormGroup) {
+
+    let invalidControls: string[] = [];
+
+    Object.keys(formGroup.controls).forEach((key) => {
+      const control = formGroup.get(key);
+      if (control && control.invalid) {
+        invalidControls.push(key);
+      }
+    });
+
+    if(invalidControls.length < 1) return
+
+    // Format the control names
+    invalidControls = invalidControls.map(item =>
+      " "+String(item).charAt(0).toUpperCase()+String(item).slice(1).replace(/(?<!^)([A-Z])/g, ' $1'))
+
+    this.parseMessage(invalidControls)
+  }
+
+  test() {
+    this.findInvalidControls(this.loanDetailsForm)
   }
 
   onSubmit() {
